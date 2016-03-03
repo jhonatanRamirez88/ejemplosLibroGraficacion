7e344ae4ca4cc6bbd874f94759da3760d02ee4a1
punki03 <tonny_jho@192.168.1.107>
2016-03-03 00:16:51 -0600

    Se modifica el paint para cuadros con transparencias

--- src/Ventana.java
+++ src/Ventana.java
@@ -39,6 +39,25 @@ public class Ventana extends JFrame{
 //    }
     
     public void paint(Graphics g){
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.red);
+        Rectangle2D r1 = new Rectangle2D.Float(250.0f, 50.0f, 100.0f, 100.0f);
+        g2.fill(r1);
+        AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.5f);
+        g2.setColor(Color.green);
+        g2.setComposite(ac);
+        Rectangle2D r2 = new Rectangle2D.Float(200.0f, 100.0f, 100.0f, 100.0f);
+        g2.fill(r2);
+        ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f);
+        g2.setColor(Color.magenta);
+        g2.setComposite(ac);
+        Rectangle2D r3 = new Rectangle2D.Float(150.0f, 150.0f, 100.0f, 100.0f);
+        g2.fill(r3);
+        ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .8f);
+        g2.setColor(Color.yellow);
+        g2.setComposite(ac);
+        Rectangle2D r4 = new Rectangle2D.Float(100.0f, 200.0f, 100.0f, 100.0f);
+        g2.fill(r4);
     }
     
     
