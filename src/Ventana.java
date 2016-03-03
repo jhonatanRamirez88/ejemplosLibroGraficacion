610b11ebdcb35e5eabc787da9829a572cd8970df
punki03 <tonny_jho@192.168.1.107>
2016-03-03 00:02:17 -0600

    solo tengo las clases Ejemplo y Ventana

---
+++ src/Ventana.java
@@ -0,0 +1,46 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tonny_jho
+ */
+public class Ventana extends JFrame{
+    public Ventana(){
+        super("Prueba de Ventana");
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+        this.setSize(250,250);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+    }
+    
+    
+    //ejemplo del cuadro con linea gruesa
+//    public void paint(Graphics g){
+//        Graphics2D g2 = (Graphics2D)g;
+//        Rectangle2D r2 = new Rectangle2D.Float(75, 50, 100, 25);
+//        //se pondra mas gruesa la linea del pincel para dibujarlo
+//        Stroke pincel = new BasicStroke(4.0f, BasicStroke.CAP_ROUND,BasicStroke.JOIN_MITER);
+//        //se pondra mas gruesa la linea del pincel para dibujarlo
+//        g2.setStroke(pincel);
+//        g2.draw(r2);
+//    }
+    
+    public void paint(Graphics g){
+    }
+    
+    
+    
+}
