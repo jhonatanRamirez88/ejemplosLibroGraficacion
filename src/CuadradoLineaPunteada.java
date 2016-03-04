c0af4b79637ea6cb127134509783b11a0990ffed
punki03 <tonny_jho@192.168.1.107>
2016-03-03 18:22:43 -0600

    clase cuadro con linea punteada y clase con gradiante empezandola

--- src/CuadradoLineaPunteada.java
+++ src/CuadradoLineaPunteada.java
@@ -1,9 +1,9 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,8 +14,8 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
  *
  * @author tonny_jho
  */
-public class Ejemplo extends JFrame{    
-    public Ejemplo(){
+public class CuadradoLineaPunteada extends JFrame{
+    public CuadradoLineaPunteada(){
         super("Prueba de Ventana");
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
@@ -24,19 +24,24 @@ public class Ejemplo extends JFrame{
         this.setVisible(true);
     }
     public static void main(String[] args) {
-        Ejemplo v = new Ejemplo();
+       CuadradoLineaPunteada  v = new CuadradoLineaPunteada();
     }
     
     
     
     public void paint(Graphics g){
         Graphics2D g2 = (Graphics2D)g;
-        Rectangle2D r2 = new Rectangle2D.Float(75,50,100,25);
-        Stroke pincel = new BasicStroke(4.0f,BasicStroke.CAP_SQUARE,BasicStroke.JOIN_ROUND);
-        g2.setStroke(pincel);
+        Rectangle2D r2 = new Rectangle2D.Float(200,150,100,25);
+        float punteo1[] = {10.0f,4.0f};
+        BasicStroke pincel1 = new BasicStroke(4.0f,BasicStroke.CAP_ROUND,BasicStroke.JOIN_MITER,3.0f,punteo1,10.0f);
+        g2.setStroke(pincel1);
         g2.draw(r2);
+        
+        Rectangle2D r3 = new Rectangle2D.Float(200, 300, 100, 25);
+        float punteo2[] = {10.0f,3.0f,10.0f,4.0f,2.0f,8.0f};
+        BasicStroke pincel2 = new BasicStroke(4.0f,BasicStroke.CAP_ROUND,BasicStroke.JOIN_MITER,3.0f,punteo2,10.0f);
+        g2.setStroke(pincel2);
+        g2.draw(r3);
+     
     }
-    
-    
-    
 }
--- src/ExGradiante.java
+++ src/ExGradiante.java
@@ -4,39 +4,35 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author tonny_jho
  */
-public class Ejemplo extends JFrame{    
-    public Ejemplo(){
+public class ExGradiante extends JFrame {
+
+    public ExGradiante() {
         super("Prueba de Ventana");
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
-        this.setSize(250,250);
+        this.setSize(250, 250);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
+
     public static void main(String[] args) {
-        Ejemplo v = new Ejemplo();
+        ExGradiante v = new ExGradiante();
     }
-    
-    
-    
-    public void paint(Graphics g){
-        Graphics2D g2 = (Graphics2D)g;
-        Rectangle2D r2 = new Rectangle2D.Float(75,50,100,25);
-        Stroke pincel = new BasicStroke(4.0f,BasicStroke.CAP_SQUARE,BasicStroke.JOIN_ROUND);
-        g2.setStroke(pincel);
-        g2.draw(r2);
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        GradientPaint gp1 = new GradientPaint();
+        
     }
-    
-    
-    
 }
